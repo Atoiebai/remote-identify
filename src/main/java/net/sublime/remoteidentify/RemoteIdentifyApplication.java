6683a6dc173bf30652e0c41d95dbd43ee0104e6d
Sublime_47 <yusubov9999@mail.ru>
2022-05-24 08:59:55 +0500

    initial commit

---
+++ src/main/java/net/sublime/remoteidentify/RemoteIdentifyApplication.java
@@ -0,0 +1,13 @@
+package net.sublime.remoteidentify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteIdentifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteIdentifyApplication.class, args);
+    }
+
+}
